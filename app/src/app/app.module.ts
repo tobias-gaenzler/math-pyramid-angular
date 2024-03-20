cc14d9ea10aa7a6e788f3df3a637f2971e2faae3
tobias-gaenzler <1056318+tobias-gaenzler@users.noreply.github.com>
2024-03-20 13:34:11 +0100

    Fix animations error

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,13 +3,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {CoreModule} from './core/core.module';
 import {AppRoutingModule} from "./app-routing.module";
+import {NoopAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
     imports: [
         BrowserModule,
         CoreModule,
         CoreModule.forRoot(),
-        AppRoutingModule
+        AppRoutingModule,
+        NoopAnimationsModule
     ],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
