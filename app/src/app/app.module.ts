540adc9aeb7b4d357d19a173887b3182b403e8d1
Tobias <1056318+tobias-gaenzler@users.noreply.github.com>
2024-03-25 14:30:09 +0100

    Style toolbar

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,13 +1,16 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppComponent} from './app.component';
-import {CoreModule} from './core/core.module';
-import {AppRoutingModule} from "./app-routing.module";
-import {NoopAnimationsModule} from "@angular/platform-browser/animations";
-import {PlayModule} from "./play/play.module";
-import {PageNotFoundModule} from "./page-not-found/page.not.found.module";
-import {HelpPageModule} from "./help-page/help.page.module";
-import {ConfigModule} from "./config/config.module";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { AppRoutingModule } from "./app-routing.module";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { PlayModule } from "./play/play.module";
+import { PageNotFoundModule } from "./page-not-found/page.not.found.module";
+import { HelpPageModule } from "./help-page/help.page.module";
+import { ConfigModule } from "./config/config.module";
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
     imports: [
@@ -19,7 +22,10 @@ import {ConfigModule} from "./config/config.module";
         PlayModule,
         PageNotFoundModule,
         HelpPageModule,
-        ConfigModule
+        ConfigModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatIconModule
     ],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
