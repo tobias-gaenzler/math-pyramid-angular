77aab110271eb2c04bd9c358dcde50c8f64de27b
gaeto001 <tobias.robert.gaenzler@externe.dfs.de>
2025-02-19 13:29:13 +0100

    refactor: improve code style and error handling in game components

--- app/src/app/play/game-solved-dialog.ts
+++ app/src/app/play/game-solved-dialog.ts
@@ -2,20 +2,20 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 export interface DialogData {
-    title: string,
-    style: string
+    title: string;
+    style: string;
 }
 
 @Component({
     selector: 'game-solved-dialog',
-    templateUrl: 'game-solved-dialog.html',
-    styleUrl: './game-solved-dialog.component.scss'
+    templateUrl: './game-solved-dialog.html',
+    styleUrls: ['./game-solved-dialog.component.scss']
 })
 export class GameSolvedDialog {
     constructor(
         public dialogRef: MatDialogRef<GameSolvedDialog>,
-        @Inject(MAT_DIALOG_DATA) public data: DialogData) {
-    }
+        @Inject(MAT_DIALOG_DATA) public data: DialogData
+    ) {}
 
     close(): void {
         this.dialogRef.close();
--- app/src/app/play/play.component.ts
+++ app/src/app/play/play.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MathPyramidModel } from "../models/math-pyramid";
 import { WebsocketService } from '../service/web-socket.service';
 import { GameService } from '../service/game.service';
@@ -9,55 +9,59 @@ import { UserService } from '../service/user.service';
 @Component({
     selector: 'app-play',
     templateUrl: './play.component.html',
-    styleUrl: './play.component.scss'
+    styleUrls: ['./play.component.scss']
 })
-export class PlayComponent {
+export class PlayComponent implements OnInit {
 
-    model: MathPyramidModel | undefined
+    model: MathPyramidModel | undefined;
 
-    constructor(private websocketService: WebsocketService,
+    constructor(
+        private websocketService: WebsocketService,
         private gameService: GameService,
         public dialog: MatDialog,
-        private userService: UserService) {
-    }
+        private userService: UserService
+    ) {}
 
     ngOnInit(): void {
         this.websocketService.addListener((event: MessageEvent<any>) => {
-            if (event.data.includes("\"action\":\"message\"")) {
-                console.log(`Received message: ${event.data}`)
-                const solvedBy = JSON.parse(event.data).sender
-                const solveTime = JSON.parse(event.data).solveTime
-                this.gameService.gameSolved(solvedBy, solveTime);
-                const title = this.getGameSolvedDialogTitle(solvedBy, solveTime);
-                this.openGameSolvedDialog(title, solvedBy)
-            } else {
-                console.log(`[${new Date().toISOString()}]: Game started, received new model`)
-                this.model = new MathPyramidModel(JSON.parse(event.data))
-                this.gameService.startGame(this.model)
-                this.dialog.closeAll()
+            try {
+                const data = JSON.parse(event.data);
+                if (data.action === 'message') {
+                    console.log(`Received message: ${event.data}`);
+                    const { sender: solvedBy, solveTime } = data;
+                    this.gameService.gameSolved(solvedBy, solveTime);
+                    const title = this.getGameSolvedDialogTitle(solvedBy, solveTime);
+                    this.openGameSolvedDialog(title, solvedBy);
+                } else {
+                    console.log(`[${new Date().toISOString()}]: Game started, received new model`);
+                    this.model = new MathPyramidModel(data);
+                    this.gameService.startGame(this.model);
+                    this.dialog.closeAll();
+                }
+            } catch (error) {
+                console.error('Error processing WebSocket message:', error);
             }
-        })
+        });
     }
 
     sendRestart(): void {
-        this.websocketService.sendRestart()
+        this.websocketService.sendRestart();
     }
 
     isReady(): boolean {
-        return this.websocketService.isReady()
+        return this.websocketService.isReady();
     }
 
     private openGameSolvedDialog(title: string, solvedBy: string): void {
         this.dialog.open(GameSolvedDialog, {
-            data: { title: title, style: this.isSolvedByCurrentPlayer(solvedBy) ? "winner" : "looser" },
-            panelClass: this.isSolvedByCurrentPlayer(solvedBy) ? "winner" : "looser"
-
+            data: { title, style: this.isSolvedByCurrentPlayer(solvedBy) ? 'winner' : 'looser' },
+            panelClass: this.isSolvedByCurrentPlayer(solvedBy) ? 'winner' : 'looser'
         });
     }
 
     private getGameSolvedDialogTitle(solvedBy: string, solveTime: number): string {
-        const formattedSolveTime = new Intl.NumberFormat(navigator.language ? navigator.language : "en-us", { minimumFractionDigits: 3 }).format(solveTime / 1000);
-        return (this.isSolvedByCurrentPlayer(solvedBy) ? "YOU" : solvedBy) + " solved the pyramid in " + formattedSolveTime + " sec!";
+        const formattedSolveTime = new Intl.NumberFormat(navigator.language || 'en-US', { minimumFractionDigits: 3 }).format(solveTime / 1000);
+        return `${this.isSolvedByCurrentPlayer(solvedBy) ? 'YOU' : solvedBy} solved the pyramid in ${formattedSolveTime} sec!`;
     }
 
     private isSolvedByCurrentPlayer(solvedBy: string): boolean {
