097dc3c04205e44614a828a46b67903a3d1c7ff9
gaeto001 <tobias.robert.gaenzler@externe.dfs.de>
2025-02-19 12:37:50 +0100

    refactor: fix style issues and improve dialog error handling in toolbar component

--- app/src/app/toolbar/toolbar.component.ts
+++ app/src/app/toolbar/toolbar.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
-import { UserService } from '../service/user.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { AppRoutingModule } from '../app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+
+import { UserService } from '../service/user.service';
+import { AppRoutingModule } from '../app-routing.module';
 import { UserNameDialog } from './user-name-dialog';
 import { WebsocketService } from '../service/web-socket.service';
-
 
 @Component({
     selector: 'toolbar',
     templateUrl: './toolbar.component.html',
-    styleUrl: './toolbar.component.scss',
+    styleUrls: ['./toolbar.component.scss'],
     standalone: true,
     imports: [
         MatToolbarModule,
@@ -25,25 +25,26 @@ import { WebsocketService } from '../service/web-socket.service';
     ]
 })
 export class ToolbarComponent {
-    title: string = 'Math Pyramid'
-    name: string
+    title: string = 'Math Pyramid';
+    name: string;
 
     constructor(private userService: UserService, public dialog: MatDialog, private websocketService: WebsocketService) {
-        this.name = userService.getUserName()
+        this.name = userService.getUserName();
     }
 
-    openDialog() {
+    openDialog(): void {
         const dialogRef = this.dialog.open(UserNameDialog, {
             data: { name: this.name }
         });
 
         dialogRef.afterClosed().subscribe(result => {
-            if (result === undefined || result === null || result === "") {
-                return
+            if (result) {
+                this.userService.setUserName(result);
+                this.websocketService.sendUserName();
+                this.name = result;
             }
-            this.userService.setUserName(result)
-            this.websocketService.sendUserName()
-            this.name = result
+        }, error => {
+            console.error('Dialog closed with error:', error);
         });
     }
 }
