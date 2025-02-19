eaad0aa4e31af06cec1373abae29524bf86bf8b1
gaeto001 <tobias.robert.gaenzler@externe.dfs.de>
2025-02-19 12:12:44 +0100

    Review

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,25 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-import { AppRoutingModule } from "./app-routing.module";
-import { NoopAnimationsModule } from "@angular/platform-browser/animations";
-import { PlayModule } from "./play/play.module";
-import { PageNotFoundModule } from "./page-not-found/page.not.found.module";
-import { HelpPageModule } from "./help-page/help.page.module";
-import { ConfigModule } from "./config/config.module";
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatDialogModule } from '@angular/material/dialog';
+
+import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 
+import { CoreModule } from './core/core.module';
+import { AppRoutingModule } from './app-routing.module';
+import { PlayModule } from './play/play.module';
+import { PageNotFoundModule } from './page-not-found/page.not.found.module';
+import { HelpPageModule } from './help-page/help.page.module';
+import { ConfigModule } from './config/config.module';
+
 @NgModule({
-    declarations: [AppComponent],
-    bootstrap: [AppComponent],
+    declarations: [
+        AppComponent
+    ],
     imports: [
         BrowserModule,
-        CoreModule,
         CoreModule.forRoot(),
         AppRoutingModule,
         NoopAnimationsModule,
@@ -30,9 +32,9 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
         MatToolbarModule,
         MatButtonModule,
         MatIconModule,
-        ToolbarComponent,
-        MatDialogModule
-    ]
+        MatDialogModule,
+        ToolbarComponent
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
